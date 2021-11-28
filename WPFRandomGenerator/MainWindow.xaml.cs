561b9498297d75a7234f5a02acc439331e9838ff
Максим Манин <maks.manin.04@gmail.com>
2021-11-28 14:50:22 +1000

    Add project files.

---
+++ WPFRandomGenerator/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFRandomGenerator;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private Random random = new Random();
+
+    private void bExit_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void bGenerate_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            int minimum = Convert.ToInt32(numberMin.Text);
+            int maximum = Convert.ToInt32(numberMax.Text);
+
+            if (minimum < maximum)
+                textNumber.Text = random.Next(minimum, maximum).ToString();
+        }
+        catch(Exception)
+        {
+            textNumber.Text = "Wrong values!";
+        }
+    }
+}
